ab30a5b004a4e8383e71329cc0d844a6af52bf88
Sova <108209320+NceSova@users.noreply.github.com>
2024-06-17 21:05:15 +0300

    UI remake

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const myLibrary = [];
+class Book {
+  constructor(title, author, pages = 0, read = false) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 
-function Book(title, author, pages = 0, read = false) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  this.info = function () {
+  info() {
     return `${title} by ${author}, ${pages} pages, ${
       read ? "already readed" : "not read yet"
     }`;
-  };
+  }
 }
 
 function addBookToLibrary(title, author, pages, read) {
@@ -30,20 +32,33 @@ const shelf = document.querySelector(".books-container");
 function updateShelf() {
   myLibrary.forEach((book) => {
     const newBook = document.createElement("div");
+    newBook.classList.add("book");
+    newBook.style.backgroundColor = `${book.read ? "#22c55e" : "#ef4444"}`;
+
+    const bookDiv = document.createElement("div");
+    bookDiv.classList.add("book-upper");
+
     const title = document.createElement("h3");
     title.textContent = book.title;
+    title.classList.add("book-title");
     const author = document.createElement("h4");
     author.textContent = book.author;
+    author.classList.add("book-author");
     const numOfPages = document.createElement("h5");
     numOfPages.textContent = `${book.pages} страницы`;
-    if (book.read) {
-      newBook.classList.add("readed");
-    } else {
-      newBook.classList.add("notReaded");
-    }
-    newBook.appendChild(author);
-    newBook.appendChild(title);
-    newBook.appendChild(numOfPages);
+    numOfPages.classList.add("book-pages");
+
+    newBook.appendChild(bookDiv);
+
+    bookDiv.appendChild(author);
+    bookDiv.appendChild(title);
+    bookDiv.appendChild(numOfPages);
+
+    const isRead = document.createElement("h4");
+    isRead.textContent = `${book.read ? "Прочитано" : "Не прочитано"}`;
+    isRead.classList.add("book-read");
+
+    newBook.appendChild(isRead);
     shelf.appendChild(newBook);
   });
 }
